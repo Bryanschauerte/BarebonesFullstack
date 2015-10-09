ca17519b537b50b0b1c3a0a3bf30441bcef9fd38
Bryan Schauerte <bryan.schauerte@gmail.com>
2015-10-09 13:17:48 -0600

    workingmessage posting and receiving

--- public/app.js
+++ public/app.js
@@ -1,13 +1,44 @@
-var app = angular.module('bareApp', [])
-
-.service('message', function(){
+var app = angular.module('bareApp', ['ngMaterial']);
 
 
-})
 
 
-.controller('mainCtrl', function($scope){
-  
+app.controller('mainCtrl', function($scope, messageService){
+
+  $scope.messages = [];
+
+$scope.getMessages = function(){
+  messageService.getMessages().then(function(messages){
+    $scope.messages = messages;
+  })
+};
+
+$scope.postMessage = function(){
+  messageService.postMessage($scope.newUser).then(function(){
+    $scope.getMessages();
+    $scope.newMessage = {};
 
 
-})
+  })
+}
+
+
+
+});
+
+app.service('messageService', function($http){
+
+  this.getMessages = function(){
+    return $http.get('/api/messages').then(function(res){
+      return res.data
+    })
+  }
+  this.postMessage = function(newMessage){
+    return $http.post('/api/messages', newMessage)
+  }
+
+
+
+
+
+});
--- server.js
+++ server.js
@@ -7,22 +7,50 @@ var express = require('express'),
 
   app.use(express.static(__dirname + '/public'));
   app.use(bodyParser.json());
-
+  app.use(cors());
 
 var mongoPort = process.env.MONGO_PORT || 27017;
 var mongoURI  = 'mongodb://localhost:' + mongoPort + '/barebones';
 
-  app.listen(port, function(){
-    console.log('listening on port ' + port);
-  });
 
 
+var MessageSchema = new mongoose.Schema({
+  message:{type: String, required: true},
+  name: {type : String, required: true}
+});
 
-var messageSchema =
-
+var Message = mongoose.model('Message', MessageSchema);
 var serverPort = process.env.EXPRESS_PORT || 8181;
 
 mongoose.connect(mongoURI);
 mongoose.connection.once('open', function(){
   console.log('mdb listening on:', mongoPort);
 });
+
+app.post('/api/messages', function(req, res){
+  Message.create(req.body, function(err, result){
+
+    if(err){
+      res.send(err);
+    }
+    else{
+      res.send(result);
+    }
+  })
+})
+
+app.get('/api/messages', function(req, res){
+  Message.find({}, function(err, result){
+    if(err){
+      res.send(err);
+    } else {
+      res.send(result)
+    }
+
+
+  })
+})
+
+app.listen(port, function(){
+    console.log('listening on port ' + port);
+  });
